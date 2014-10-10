905ec9a7c0dce05e20b9d8b6e3035aecda187410
Juvenal Bisneto <juvenal.bisneto@gmail.com>
2014-10-10 19:54:19 -0300

    Atualizando GrammarSymbols

--- src/parser/GrammarSymbols.java
+++ src/parser/GrammarSymbols.java
@@ -11,21 +11,15 @@ package parser;
 public class GrammarSymbols {
 
 	// Language terminals (starts from 0)
-	public static final int EOF = 0, IDENTIFIER = 1, NUMBER = 2, BOOL_VALUE = 3, PIC9_TYPE = 4, PICBOOL_TYPE = 5, VOID = 6,
-			OP_RELACIONAL = 10, OP_ADD = 11, OP_MULT = 12, L_PAR = 13, R_PAR = 14, POINT = 15,
-			MAJOR = 20, MAJOREQ = 21, MINOR = 22, MINOREQ = 23, NOT = 24, DIFFERENT = 25,
+	public static final int EOF = 0, IDENTIFIER = 1, NUMBER = 2, BOOL_VALUE = 3, PIC9_TYPE = 4, PICBOOL_TYPE = 5, VOID_TYPE = 6,
+			
+			OP_RELACIONAL = 10, OP_ADD = 11, OP_MULT = 12, L_PAR = 13, R_PAR = 14, POINT = 15, COMMA = 16,
+			MAJOR = 20, MAJOREQ = 21, MINOR = 22, MINOREQ = 23, DIFFERENT = 24, EQUALS = 25,
 			
 			ACCEPT = 30, FROM = 31, COMPUTE = 32, VALUE = 33, CALL = 34, USING = 35, DISPLAY = 36, RETURN = 37,
 			
 			IF = 40, THEN = 41, ELSE = 42, END_IF = 43,
 			PERFORM = 50, UNTIL = 51, END_PERFORM = 52, BREAK = 53, CONTINUE = 54,
 			GLOBALDATA = 60, PROGRAM = 61, DIVISION = 62, MAIN = 63, END_MAIN = 64, END_FUNCTION = 65;
-
-
-
-
-
-	//TODO
-	//public static final int EOT = 1000;
 	
 }
