d1589498dc2cda7fb323d443620d8fc205a81360
ipubi15 <ipubi15@gmail.com>
2014-10-14 00:16:07 -0300

    BooleanExpression
    Construtores feitos. i guess

--- src/util/AST/BooleanExpression.java
+++ src/util/AST/BooleanExpression.java
@@ -23,10 +23,95 @@ public class BooleanExpression extends Expression {
 		this.opr=opr;
 		this.bexpression_r=bexpression_r;
 	}
+	public BooleanExpression(BooleanExpression bexpression_l,OpRelational opr, ArithmeticExpression aexpression_r){
+		this.bexpression_l=bexpression_l;
+		this.opr=opr;
+		this.aexpression_r=aexpression_r;
+	}
+	public BooleanExpression(BooleanExpression bexpression_l,OpRelational opr, Identifier id_r){
+		this.bexpression_l=bexpression_l;
+		this.opr=opr;
+		this.id_r=id_r;
+	}
+	public BooleanExpression(BooleanExpression bexpression_l,OpRelational opr, Number numero_r){
+		this.bexpression_l=bexpression_l;
+		this.opr=opr;
+		this.numero_r=numero_r;
+	}
+
+	public BooleanExpression(ArithmeticExpression aexpression_l,OpRelational opr, BooleanExpression bexpression_r){
+		this.aexpression_l=aexpression_l;
+		this.opr=opr;
+		this.bexpression_r=bexpression_r;
+	}
+
+	public BooleanExpression(ArithmeticExpression aexpression_l,OpRelational opr, ArithmeticExpression aexpression_r){
+		this.aexpression_l=aexpression_l;
+		this.opr=opr;
+		this.aexpression_r=aexpression_r;
+	}
+	public BooleanExpression(ArithmeticExpression aexpression_l,OpRelational opr, Identifier id_r){
+		this.aexpression_l=aexpression_l;
+		this.opr=opr;
+		this.id_r=id_r;
+	}
+	public BooleanExpression(ArithmeticExpression aexpression_l,OpRelational opr, Number numero_r){
+		this.aexpression_l=aexpression_l;
+		this.opr=opr;
+		this.numero_r=numero_r;
+	}
+
+
+	public BooleanExpression(Identifier id_l,OpRelational opr, BooleanExpression bexpression_r){
+		this.id_l=id_l;
+		this.opr=opr;
+		this.bexpression_r=bexpression_r;
+	}
+	public BooleanExpression(Identifier id_l,OpRelational opr, ArithmeticExpression aexpression_r){
+		this.id_l=id_l;
+		this.opr=opr;
+		this.aexpression_r=aexpression_r;
+	}
+	public BooleanExpression(Identifier id_l,OpRelational opr, Identifier id_r){
+		this.id_l=id_l;
+		this.opr=opr;
+		this.id_r=id_r;
+	}
+	public BooleanExpression(Identifier id_l,OpRelational opr, Number numero_r){
+		this.id_l=id_l;
+		this.opr=opr;
+		this.numero_r=numero_r;
+	}
+
+
+	public BooleanExpression(Number numero_l,OpRelational opr, BooleanExpression bexpression_r){
+		this.numero_l=numero_l;
+		this.opr=opr;
+		this.bexpression_r=bexpression_r;
+	}
+	public BooleanExpression(Number numero_l,OpRelational opr, ArithmeticExpression aexpression_r){
+		this.numero_l=numero_l;
+		this.opr=opr;
+		this.aexpression_r=aexpression_r;
+	}
+	public BooleanExpression(Number numero_l,OpRelational opr, Identifier id_r){
+		this.numero_l=numero_l;
+		this.opr=opr;
+		this.id_r=id_r;
+	}
+	public BooleanExpression(Number numero_l,OpRelational opr, Number numero_r){
+		this.numero_l=numero_l;
+		this.opr=opr;
+		this.numero_r=numero_r;
+	}
+
 
 	public BooleanExpression(BoolValue bvalue){
 		this.bvalue=bvalue;
 	}
 
+
+
+//nem sei se ta certo e faltam gets
 	
 }
