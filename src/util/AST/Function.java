3be359e2c8dcdec9c96f378413894d7e69e5e656
Juvenal Bisneto <juvenal.bisneto@gmail.com>
2014-10-14 17:45:32 -0300

    AST Function

--- src/util/AST/Function.java
+++ src/util/AST/Function.java
@@ -3,9 +3,24 @@ package util.AST;
 import java.util.*;
 
 public class Function extends AST {	
-	String id;
-
-
+	String tipoRetorno;
+	Identifier id;
+	ArrayList<Identifier> params;
+	ArrayList<VarDeclaration> declarations;
+	ArrayList<Command> cmds;
 	
-
+	public Function(String tipoRetorno, Identifier id, ArrayList<Identifier> params, ArrayList<VarDeclaration> declarations, ArrayList<Command> cmds) {
+		super();
+		this.tipoRetorno = tipoRetorno;
+		this.id = id;
+		this.params = params;
+		this.declarations = declarations;
+		this.cmds = cmds;
+	}
+	
+	@Override
+	public String toString(int level) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
