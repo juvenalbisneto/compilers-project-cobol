99154188fb8ee0bf123d2c164c08e0a1a7d5441a
Juvenal Bisneto <juvenal.bisneto@gmail.com>
2014-10-14 18:08:52 -0300

    AST GlobalDataDiv

--- src/util/AST/GlobalDataDiv.java
+++ src/util/AST/GlobalDataDiv.java
@@ -1,17 +1,21 @@
 package util.AST;
 
-import java.util.*;
+import java.util.ArrayList;
 
 public class GlobalDataDiv extends AST {
 	ArrayList<VarDeclaration> varD = null;
 
 	public GlobalDataDiv (ArrayList<VarDeclaration> varD){
-		this.varD=varD;
+		this.varD = varD;
 	}
 
 	public ArrayList<VarDeclaration> getVarDeclaration(){
 		return this.varD;
 	}
-	
 
+	@Override
+	public String toString(int level) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
