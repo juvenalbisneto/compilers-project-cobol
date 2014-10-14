9839d667d17f3e61fce742845caee25bbdc3a519
ipubi15 <ipubi15@gmail.com>
2014-10-14 18:22:14 -0300

    Return e corrigindo display

--- src/util/AST/Return.java
+++ src/util/AST/Return.java
@@ -2,21 +2,22 @@ package util.AST;
 
 import java.util.*;
 
-public class Display extends Command {
+public class Return extends Command {
 	Identifier id = null;
 	Expression exp = null;
 
-	public Display(Identifier id){
+	public Return(Identifier id){
 		this.id = id;
 	}
-	public Display(Expression exp){
+	public Return(Expression exp){
 		this.exp = exp;
-	}
-
+	}	
+	
 	public Identifier getIdentifier(){
 		return this.id;
 	}
 	public Expression getExpression(){
 		return this.exp;
 	}
+}
 }
