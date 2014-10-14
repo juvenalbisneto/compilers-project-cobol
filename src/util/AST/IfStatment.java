9bfc5a544459cc833c7e0f739e35933d5de54f6c
ipubi15 <ipubi15@gmail.com>
2014-10-14 18:11:14 -0300

    Ifstatment para AST

--- src/util/AST/IfStatment.java
+++ src/util/AST/IfStatment.java
@@ -3,6 +3,27 @@ package util.AST;
 import java.util.*;
 
 public class IfStatment extends Command {
+	BooleanExpression bexpression = null;
+	ArrayList<Command> cmd_if = null;
+	ArrayList<Command> cmd_else = null;
+
+	public IfStatment(BooleanExpression bexpression, ArrayList<Command> cmd_if){
+		this.cmd_if = cmd_if;
+		this.bexpression = bexpression;
+	}
+	public IfStatment(BooleanExpression bexpression, ArrayList<Command> cmd_if, ArrayList<Command> cmd_else){
+		this.cmd_if = cmd_if;
+		this.bexpression = bexpression;
+		this.cmd_else = cmd_else;
+	}
 	
-	
+	public BooleanExpression getBooleanExpression(){
+		return this.bexpression;
+	}
+	public ArrayList<Command> getCommandIF(){
+		return this.cmd_if;
+	}
+	public ArrayList<Command> getCommandElse(){
+		return this.cmd_else;
+	}
 }
