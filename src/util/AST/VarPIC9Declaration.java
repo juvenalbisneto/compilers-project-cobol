643155f6675480414406be9692fb0bad72a30c87
Juvenal Bisneto <juvenal.bisneto@gmail.com>
2014-10-14 22:35:23 -0300

    AST Declarações de variáveis

--- src/util/AST/VarPIC9Declaration.java
+++ src/util/AST/VarPIC9Declaration.java
@@ -1,16 +1,39 @@
 package util.AST;
 
-import java.util.*;
-
 public class VarPIC9Declaration extends VarDeclaration {
+	int local;
+	String type;
 	Identifier id;
+	Number num = null;
 
-	public VarPIC9Declaration(Identifier id){
-		this.id=id;
+	public VarPIC9Declaration(int local, String type, Identifier id){
+		this.local = local;
+		this.type = type;
+		this.id = id;
+	}
+	public VarPIC9Declaration(int local, String type, Identifier id, Number num){
+		this.local = local;
+		this.type = type;
+		this.id = id;
+		this.num = num;
 	}
 
-	public getIdentifier(){
+	public int getLocal(){
+		return this.local;
+	}
+	public String getType(){
+		return this.type;
+	}
+	public Identifier getIdentifier(){
 		return this.id;
 	}
+	public Number getNumber(){
+		return this.num;
+	}
 
+	@Override
+	public String toString(int level) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- src/util/AST/VarPICBOOLDeclaration.java
+++ src/util/AST/VarPICBOOLDeclaration.java
@@ -1,16 +1,39 @@
 package util.AST;
 
-import java.util.*;
-
 public class VarPICBOOLDeclaration extends VarDeclaration {
+	int local;
+	String type;
 	Identifier id;
+	Number num = null;
 
-	public VarPICBOOLDeclaration(Identifier id){
-		this.id=id;
+	public VarPICBOOLDeclaration(int local, String type, Identifier id){
+		this.local = local;
+		this.type = type;
+		this.id = id;
+	}
+	public VarPICBOOLDeclaration(int local, String type, Identifier id, Number num){
+		this.local = local;
+		this.type = type;
+		this.id = id;
+		this.num = num;
 	}
 
-	public getIdentifier(){
+	public int getLocal(){
+		return this.local;
+	}
+	public String getType(){
+		return this.type;
+	}
+	public Identifier getIdentifier(){
 		return this.id;
 	}
+	public Number getNumber(){
+		return this.num;
+	}
 
+	@Override
+	public String toString(int level) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
