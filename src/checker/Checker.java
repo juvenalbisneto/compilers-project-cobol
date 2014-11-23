dd2c9d9cddbae8226406b033ea475ef5b6fa9312
Juvenal Bisneto <juvenal.bisneto@gmail.com>
2014-11-23 19:13:19 -0300

    Tirando a lista de TO-DOs do documento

--- src/checker/Checker.java
+++ src/checker/Checker.java
@@ -1,21 +1,10 @@
 package checker;
 
 import java.util.ArrayList;
-
 import util.AST.*;
 import util.AST.AST.Types;
 import util.AST.Number;
 import util.symbolsTable.IdentificationTable;
-
-
-
-// TODO
-// argumentos de funcoes ainda sao vistos como nao declarados. erro apontado em teste_func_return_wrong_type
-
-
-
-
-
 
 public class Checker implements Visitor{
 	private IdentificationTable idTable;
