b365a310dc35135bfa6015064e56cedb44e4b59a
Juvenal Bisneto <juvenal.bisneto@gmail.com>
2014-11-30 17:08:44 -0300

    Corrigindo erro dos números quando apareciam em boolexp

---
+++ tests/Entrega 3/run - pasta atual.bat
@@ -0,0 +1,24 @@
+@echo off
+cd C:\Users\Juvenal Bisneto\AppData\Local\nasm\
+cls
+echo # Inicializando... #
+set batchPath=%~dp0
+cls
+echo.
+echo		Execucao do Programa - Projeto de Compiladores - COBOL
+echo				by Juvenal Bisneto
+echo.
+echo.
+echo # GERANDO OBJETO #
+nasm -f win32 "%batchPath%program.asm" -o "%batchPath%program.o"
+echo.
+echo # GERANDO EXECUTAVEL #
+gcc "%batchPath%program.o" -o "%batchPath%program.exe"
+echo.
+echo # EXECUCAO #
+"%batchPath%program.exe"
+echo # FIM #
+del "%batchPath%program.o"
+del "%batchPath%program.exe"
+set batchPath=
+pause
